8b3a9f9cc5c9659372c32b99fe2d3967a4ebc8ea
Diego Rubio <dbiosag@gmail.com>
2020-11-05 23:13:21 +0100

    Fixed @StepScope of batch beans

--- src/main/java/com/exaple/batch/demospringbatch/PeopleBatchConfig.java
+++ src/main/java/com/exaple/batch/demospringbatch/PeopleBatchConfig.java
@@ -11,12 +11,14 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
 import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -25,7 +27,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManagerFactory;
 import java.util.Date;
@@ -51,26 +52,29 @@ public class PeopleBatchConfig {
 
     /* JpaPagingItemReader */
     @Bean
-    public ItemReader<Book> itemReader() {
+    @StepScope
+    public JpaPagingItemReader<Book> itemReader() {
         val jpqlQuery = "select p from Book p";
-        return new JpaPagingItemReaderBuilder<Book>()
+        JpaPagingItemReader itemReader = new JpaPagingItemReaderBuilder<Book>()
                 .name("itemReader")
                 .entityManagerFactory(entityManagerFactory)
                 .queryString(jpqlQuery)
                 .pageSize(1)
                 .build();
+        return itemReader;
     }
 
     /* JpaItemWriter */
-    @Transactional
     @Bean
-    public ItemWriter<Book> itemWriter() {
+    @StepScope
+    public JpaItemWriter<Book> itemWriter() {
         JpaItemWriter<Book> employeeJpaItemWriter = new JpaItemWriter<>();
         employeeJpaItemWriter.setEntityManagerFactory(entityManagerFactory);
         return employeeJpaItemWriter;
     }
 
     @Bean
+    @StepScope
     public ItemProcessor<Book, Book> itemProcessor() {
         return new PeopleItemProcessor();
     }
